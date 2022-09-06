e648cae139300d97eb85e0f4bf8444492f0744ed
fsampson30 <fsampson30@gmail.com>
2022-09-06 07:44:55 -0300

    Initiated LandingPage.

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Image } from "react-native";
+import styles from "./syyles";
+
+import landingImg from '../../assets/images/landing.png';
+
+function Landing() {
+    return (
+        <View style={styles.container}>
+            <Image source={landingImg} style={styles.banner}/>
+
+
+        </View>
+    )
+
+}
+
+export default Landing
