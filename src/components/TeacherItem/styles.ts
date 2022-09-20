fbcea3adadadd887c977bfc103df145f6a6d28ff
fsampson30 <fsampson30@gmail.com>
2022-09-20 07:53:35 -0300

    Styles changes.

--- src/components/TeacherItem/styles.ts
+++ src/components/TeacherItem/styles.ts
@@ -1,3 +1,5 @@
+import { Archivo_700Bold } from "@expo-google-fonts/archivo";
+import { Poppins_400Regular } from "@expo-google-fonts/poppins";
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
@@ -17,19 +19,30 @@ const styles = StyleSheet.create({
     },
 
     avatar: {
-
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        backgroundColor: '#EEE'
     },
 
     profileInfo: {
+        marginLeft: 16
 
     },
 
     name: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#32264D',
+        fontSize: 20
 
     },
 
     subject: {
-        
+        fontFamily: 'Poppins_400Regular',
+        color: '#6A6180',
+        fontSize: 12,
+        marginTop: 4
+
     }
 
 })
